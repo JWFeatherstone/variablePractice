73a6a2e70c47795d11d71b8dad3f6ba7d222a4d9
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-01-10 12:23:05 -0700

    Reassign strings and add print variable statements

--- variables.js
+++ variables.js
@@ -1,9 +1,15 @@
-var name = "John"
+var nameStudent = "John"
 var exercise = "Data Types"
-var status = "Tab overload"
+var statusStudent = "Tab overload"
 var age = 29
 var session = 2
 var smallGroup = 11
 var terminalFinished = true
 var pairingFinished = false
 var loomAnxiety = true
+nameStudent = "Other"
+exercise = "Pairing"
+statusStudent = "Hopefully finished"
+console.log(nameStudent)
+console.log(age)
+console.log(pairingFinished)
